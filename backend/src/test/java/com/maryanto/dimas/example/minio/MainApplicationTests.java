947bdb10bbaf78463c8ac1d8444de0f60cd8decb
dimasm93 <software.dimas_m@icloud.com>
2022-03-28 11:24:33 +0700

    init projects

---
+++ backend/src/test/java/com/maryanto/dimas/example/minio/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example.minio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
